3cd4a18e04723b6ece6d5481a96b6b550b91fc9c
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2023-12-23 08:11:14 +0200

    init project

---
+++ scriptJs.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const video = document.getElementById("CameraId")
+    navigator.mediaDevices.getUserMedia({video: true})
+        .then(function(stream){video.srcObject= stream})
+        .catch(function(error){console.error('Error accession the camera: ', error);})
+        
+})
