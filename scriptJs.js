648b28d9082ffb10472fd4bcc01624c511e3b098
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2023-12-23 08:28:07 +0200

    font Ubuntu

--- scriptJs.js
+++ scriptJs.js
@@ -2,6 +2,5 @@ document.addEventListener("DOMContentLoaded", function(){
     const video = document.getElementById("CameraId")
     navigator.mediaDevices.getUserMedia({video: true})
         .then(function(stream){video.srcObject= stream})
-        .catch(function(error){console.error('Error accession the camera: ', error);})
-        
+        .catch(function(error){console.error('Error accession the camera: ', error);}) 
 })
